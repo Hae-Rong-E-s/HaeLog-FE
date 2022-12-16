7628e55288dd2feafecf940007316b4db6933ab2
Suebin An <jejupalette@naver.com>
2022-12-16 22:29:14 +0900

    feat : layout 적용

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/main";
-import Login from "../pages/login";
-import Detail from "../pages/detail";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Detail from "../pages/Detail";
+import Layout from "../components/layout/Layout";
 
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/detail/:id" element={<Detail />} />
+          </Route>
           <Route path="/login" element={<Login />} />
-          <Route path="/detail/:id" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </>
