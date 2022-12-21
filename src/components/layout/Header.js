ec01e97c4f0df8e10e54cea9b70ff273b34c7e0a
Suebin An <jejupalette@naver.com>
2022-12-21 16:28:08 +0900

    fix : header 마진 패딩 수정

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,34 +3,6 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Button from "../elem/Button";
 
-const Container = styled.div`
-  padding: 20px;
-  width: 100vw;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0;
-`;
-
-const MainTitle = styled.div`
-  color: #ffffff;
-  font-size: 20px;
-  font-weight: 900;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  div {
-    margin-left: 15px;
-  }
-`;
-
-const HIcon = styled.img`
-  width: 30px;
-  background-color: transparent;
-  filter: invert(15%) sepia(28%) saturate(6934%) hue-rotate(351deg)
-    brightness(99%) contrast(90%);
-`;
-
 const Header = () => {
   const navigate = useNavigate();
   return (
@@ -46,9 +18,44 @@ const Header = () => {
         ></HIcon>
         <div> Tom's Healog</div>
       </MainTitle>
-      <Button>로그인</Button>
+      <Button
+        margin="0 20px"
+        onClick={() => {
+          navigate("login");
+        }}
+      >
+        로그인
+      </Button>
     </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 30px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MainTitle = styled.div`
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 900;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 0 20px;
+  div {
+    margin-left: 15px;
+  }
+`;
+
+const HIcon = styled.img`
+  width: 30px;
+  background-color: transparent;
+  filter: invert(15%) sepia(28%) saturate(6934%) hue-rotate(351deg)
+    brightness(99%) contrast(90%);
+`;
 
 export default Header;
