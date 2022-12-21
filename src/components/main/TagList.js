2d09a1c1e7917382155775772e2dd148ed6da02c
DahyeJang <everything888@naver.com>
2022-12-21 09:49:23 +0900

    feat: 코드 수정

--- src/components/main/TagList.js
+++ src/components/main/TagList.js
@@ -6,6 +6,36 @@ const Container = styled.div`
   margin: 30px 0;
 `;
 
+const mokdata = {
+  result: "success",
+  msg: "유저의 게시물 조회 성공",
+  data: [
+    {
+      profileImage: "http//~~", //유저 프로필 이미지 주소
+      description: "유저 한줄 소개",
+      postid: "1",
+      createAt: "2022-12-16",
+      title: "게시글 제목1",
+      tags: ["CS", "SpringBoot"],
+      contentSummary: "본문 150자 요약",
+      nickname: "jaeHyunKing",
+      commentCount: "3",
+    },
+    {
+      profileImage: "http//~~", //유저 프로필 이미지 주소
+      description: "유저 한줄 소개",
+      postid: "2",
+      createAt: "2022-12-16",
+      title: "게시글 제목2",
+      tags: ["react", "html"],
+      contentSummary: "본문 150자 요약",
+      nickname: "jaeHyunKing",
+      commentCount: "2",
+    },
+  ],
+};
+console.log(mokdata.data);
+
 const TagList = () => {
   return (
     <Container>
