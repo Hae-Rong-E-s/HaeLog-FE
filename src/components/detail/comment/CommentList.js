4d9a3f827eac24f46dbd248d7f0f9a19f3ee7e4d
dkaodkaork <dkaodkaork@gmail.com>
2022-12-21 22:01:09 +0900

    feat: 댓글 기능 구현 코드 수정중

--- src/components/detail/comment/CommentList.js
+++ src/components/detail/comment/CommentList.js
@@ -3,25 +3,17 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { __getComment } from "../../../redux/modules/commentSlice";
 import UserInfoContainer from "./UserInfoContainer";
-// 댓글 수정 부분
-//1. 내가 작성한 댓글에만 버튼이 노출되도록 -> 백으로 부터 boolen 데이터를 받기로했다 -> true면 버튼이 노출되도록 false 면 버튼 노출 X
-//2. 댓글 수정 버튼 클릭시 -> 해당 댓글의 textarea 활성화 (textarea 활성화 비활성화 상태 관리 하나 필요 ) -> textarea에 입력하는 값 onchange event.target.value 로 받아와서
-// -> 받아온 값을 새로운 상태에 넣어서 patch 보냄 (새로 수정한 comment에 대한 상태 관리 하나 필요 )
-// 그럼 이 과정에서 어떠한 로직이 필요하냐? -> 수정하려는 그 코멘트와( onChange로  id 값 가져와서  ) useSeletor로 comments들을 싹 가져와서 그중에 수정하려는 코멘트의 id 값을 찾아서
-// {변경후} >
 
 const CommentList = ({ params, state }) => {
   //commentSlice Store에 저장되있는 값들
   const { isLoading, error, comments } = state;
 
-  console.log(comments);
-  // const comments = "";
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(__getComment(params));
   }, [dispatch, params]);
-
+  console.log(comments);
   return (
     <div>
       {comments &&
@@ -30,7 +22,7 @@ const CommentList = ({ params, state }) => {
             <div key={comment.commentId}>
               <UserInfoContainer
                 username={comment.commentMemberNickname}
-                createAt={comment.createAt}
+                createAt={comment.createAt.slice(0, 10)}
                 width="90px"
                 height="90px"
                 profileFontSize="18px"
--- src/redux/modules/commentSlice.js
+++ src/redux/modules/commentSlice.js
@@ -13,7 +13,6 @@ export const __getComment = createAsyncThunk(
   "getComment",
   async (payload, thunkAPI) => {
     try {
-      console.log(payload);
       const data = await baseURL.get(
         `api?nickname=${payload.nickname}&postid=${payload.postId}`
       );
@@ -32,7 +31,7 @@ export const __postComment = createAsyncThunk(
 
     try {
       const data = await baseURLApi.post(`/comment/${postId}`, addComment);
-      return thunkAPI.fulfillWithValue(data);
+      return thunkAPI.fulfillWithValue(addComment);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -57,8 +56,8 @@ export const __putComment = createAsyncThunk(
     console.log(payload);
     const { content, commentid } = payload;
     try {
-      const data = await baseURLApi.put(`/comment/${commentid}`, content);
-      return thunkAPI.fulfillWithValue(data.data);
+      const data = await baseURLApi.put(`/comment/${commentid}`, content); // data는 무엇이냐? => put요청으로 부터 받는 Response 값이 아닌가? => 그러면 그냥 통신 잘됬다는 메세지?
+      return thunkAPI.fulfillWithValue(data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -86,18 +85,16 @@ export const commentsSlice = createSlice({
     },
     // 댓글 작성
     [__postComment.pending]: (state) => {
-      console.log(state);
       state.isLoading = true;
     },
     [__postComment.fulfilled]: (state, action) => {
       state.isLoading = false;
-      alert(action.payload.data.msg);
-      console.log("state:", state);
-      console.log("action:", action);
+      // => 댓글 추가가 바로 반영되지 않는 문제 때문에
+      // 댓글 추가하는 원리가 궁금하다 나는 게시글의 아이디와 바뀐 댓글의 값만 보내주는데
+      // 추가한 댓글을 어떻게 리덕스에 업데이트 해야하나? => 댓글을 추가할때 post요청을 하고 요청에 대한 응답으로 받는 것이 업고, payload값은 댓글의 content 하나이고 comments는
     },
     [__postComment.rejected]: (state, action) => {
       state.isLoading = false;
-      console.log(state, action);
       state.error = action.payload;
     },
     // 댓글 삭제
@@ -108,7 +105,7 @@ export const commentsSlice = createSlice({
       state.isLoading = false;
       state.comments = state.comments.filter(
         (comment) => comment.commentId !== action.payload
-      );
+      ); // 댓글 삭제 로직 => commentId 체크해보기
     },
     [__delComment.rejected]: (state, action) => {
       state.isLoading = false;
