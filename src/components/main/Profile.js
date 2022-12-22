7d3bf2e28ae3787524ef4f8aaac391a4a64e9109
DahyeJang <everything888@naver.com>
2022-12-22 11:09:10 +0900

    feat: 코드 수정

--- src/components/main/Profile.js
+++ src/components/main/Profile.js
@@ -36,20 +36,17 @@ const Profile = () => {
   }, [dispatch]);
 
   const myInfo = useSelector((state) => state.myPage.data);
-  console.log("myInfo", myInfo);
+  //console.log("myInfo", myInfo);
 
   return (
     <Container>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJEAm6C-SVkvqJQ4_eMz0_KcL3wTuKHo-wYQ&usqp=CAU"
-        // src={myInfo[0].profileImage}
         alt="profileimg"
       ></img>
       <div>
         <div>{param.nickname}</div>
         <div>{myInfo[0]?.description}</div>
-        {/* <div>작성자</div>
-        <div>나의 코딩 블로그</div> */}
       </div>
     </Container>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,11 @@
 import React from "react";
-import ContentList from "../components/main/ContentList";
 import Profile from "../components/main/Profile";
-import TagList from "../components/main/TagList";
 import MyPage from "../components/main/MyPage";
 
 const Main = () => {
   return (
     <div>
       <Profile />
-      {/* <TagList />
-      <ContentList /> */}
       <MyPage />
     </div>
   );
